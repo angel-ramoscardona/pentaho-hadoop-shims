63dbb1227cdad827491ca0f3ff1bf2ba4d3aaa5c
Angel Ramos <angel.ramos@hitachivantara.com>
2018-09-20 14:06:17 -0400

    [BACKLOG-25512] Started common services api flattening process - See pentaho-hadoop-shims-common-services-api / 1flatten

---
+++ common-services-api/1flatten/src/main/java/org/pentaho/hadoop/shim/api/format/IPentahoAvroInputFormat.java
@@ -0,0 +1,62 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2018 by Hitachi Vantara : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+package org.pentaho.hadoop.shim.api.format;
+
+
+import java.io.InputStream;
+import java.util.List;
+
+public interface IPentahoAvroInputFormat extends IPentahoInputFormat {
+
+  /**
+   * Set schema for file reading.
+   */
+  List<? extends IAvroInputField> getFields(  ) throws Exception;
+
+  /**
+     * Set schema for file reading.
+     */
+  void setInputFields( List<? extends IAvroInputField> fields ) throws Exception;
+
+  /**
+   * Set input file.
+   */
+  void setInputFile( String file ) throws Exception;
+
+  /**
+   * Set input file.
+   */
+  void setInputSchemaFile( String schemaFile ) throws Exception;
+
+  /**
+   * Split size, bytes.
+   */
+  void setSplitSize( long blockSize ) throws Exception;
+
+  String getInputStreamFieldName();
+
+  void setInputStreamFieldName( String inputStreamFieldName );
+
+  boolean isUseFieldAsInputStream();
+
+  void setInputStream( InputStream inputStream );
+}
